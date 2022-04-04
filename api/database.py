3bc7ed6fcb437fd79134f156256c733889c2fbd0
Nova Trauben <traubenn@wit.edu>
2022-04-04 13:01:44 -0400

    Transfering to heroku postgres

--- api/database.py
+++ api/database.py
@@ -26,8 +26,10 @@ else:
     raise KeyError("Error: Host args > 2")
 
 try:
-    engine = create_engine(os.environ.get('DATABASE_URL'))
-    print("here")
+    db_url = os.environ.get('DATABASE_URL')
+    print(db_url)
+
+    engine = create_engine(db_url)
 except Exception:
     engine = create_engine(engine.url.URL.create(drivername="postgresql+psycopg2", username=db_user, password=db_pass,
                                                  host=db_hostname, port=db_port, database=db_name))
